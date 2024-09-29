3f8d8ecd858f23ad00b6e22b7682fe2cc9d6adc7
llllbhw <132913797+llllbhw@users.noreply.github.com>
2024-09-29 16:23:36 +0800

    Update init.c

--- lab1/kern/init/init.c
+++ lab1/kern/init/init.c
@@ -32,7 +32,9 @@ int kern_init(void) {
     clock_init();  // init clock interrupt
 
     intr_enable();  // enable irq interrupt
-    
+
+    asm("mret");
+    asm("ebreak");
     while (1)
         ;
 }
